739b1b1552440c254bbe67507773664a898dc1f7
blind <2318355269@qq.com>
2025-03-29 14:15:17 +0800

    【功能修改】hello 提交测试 【开发周期】0.1H 【修改人】duo luo luo 【评审人】duo luo luo 【提交人】duo luo luo

--- hello/src/main/java/com/hello/HelloController.java
+++ hello/src/main/java/com/hello/HelloController.java
@@ -4,12 +4,17 @@ package com.hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/hello")
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/say")
     public String hello() {
         return "Hello World";
     }
+
+    public static void main(String[] args) {
+
+    }
+
 
 }
